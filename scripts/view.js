22bb981f2a72d363dc6d40f6b2362e157e74db45
Domingos Nkula Pedro <domingosnkulajw@gmail.com>
2021-09-17 07:55:09 +0100

    Update view page

--- scripts/view.js
+++ scripts/view.js
@@ -2,3 +2,7 @@ let linkMenu = document.querySelectorAll("#list-menu a")
 let inputCheckMenu = document.querySelector("#check-mobile")
 
 for(link of linkMenu){ link.onclick = () => {inputCheckMenu.checked = false} }
+
+function goTop(){
+	document.documentElement.scrollTop = 0;
+}
